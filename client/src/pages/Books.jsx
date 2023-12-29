f8779f4fb561d7a0a164f0e27248f77fe3d88f40
cywin1018 <cywin0821@gmail.com>
2023-12-29 18:17:00 +0900

    finish coding

---
+++ client/src/pages/Books.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import axios from "axios"
+
+const Books = () => {
+  const [ books,setBooks] = useState([])
+
+  useEffect(()=>{
+    const fecthAllBooks = async ()=>{
+      try{
+        const res = await axios.get("http://localhost:8800/books")
+        
+        setBooks(res.data)
+        console.log(res)
+      }catch(err){
+        console.log(err)
+      }
+    }
+    fecthAllBooks()
+  },[])
+
+  const handleDelete = async (id) =>{
+    try {
+      await axios.delete("http://localhost:8800/books/"+id);
+      window.location.reload()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <div>
+    <h1>Lama Book shop</h1>
+    <div className='books'>
+      {books.map(book =>(
+        <div className="book" key={book.id}>
+          {book.cover&&<img src={book.cover} alt=""/>}
+          <h2>{book.title}</h2>
+          <p>{book.desc}</p>
+          <span>
+            {book.price}
+          </span>
+          <button className="delete" onClick={()=>handleDelete(book.id)}>Delete</button>
+          <button className="update"><Link to={`/update/${book.id}`}>Update</Link></button>
+        </div>
+      ))}
+    </div>
+    <button><Link to="/add">Add new book</Link></button>
+    </div>
+  )
+}
+
+export default Books
